79d8908a95af6237eb40b277047194ca195f1082
Md Jahed Miah <mdjahedahmed666>
2021-12-31 00:27:37 +0600

    adding animation in custom software

--- src/components/CustomSoftware.js
+++ src/components/CustomSoftware.js
@@ -9,6 +9,9 @@ import { Link } from "react-router-dom";
 import bulb from "../assets/bulb.svg";
 import stopWatch from "../assets/stopwatch.svg";
 import cash from "../assets/cash.svg";
+import documentsAnimation from "../animations/documentsAnimation/data";
+import scaleAnimation from "../animations/scaleAnimation/data.json";
+import Lottie from "react-lottie";
 
 const useStyle = makeStyles((theme) => ({
   heading: {
@@ -23,92 +26,175 @@ const useStyle = makeStyles((theme) => ({
     paddingTop: "2em",
     paddingBottom: "5em",
   },
+  itemContainer: {
+    maxWidth: "40em",
+  },
 }));
 
 const CustomSoftware = (props) => {
   const classes = useStyle();
   const theme = useTheme();
+
+  const documentOptions = {
+    loop: true,
+    autoplay: false,
+    animationData: documentsAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  const scaleOptions = {
+    loop: true,
+    autoplay: false,
+    animationData: scaleAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
   return (
-      <Grid container direction="column" className={classes.mainContainer}>
-        <Grid item container direction="row">
-          <Grid
-            item
-            className={classes.arrowContainer}
-            style={{ marginRight: "1em", marginLeft: "-3.5em" }}
+    <Grid container direction="column" className={classes.mainContainer}>
+      <Grid item container direction="row">
+        <Grid
+          item
+          className={classes.arrowContainer}
+          style={{ marginRight: "1em", marginLeft: "-3.5em" }}
+        >
+          <IconButton
+            component={Link}
+            to="/services"
+            onClick={() => props.setSelectedIndex(0)}
           >
-            <IconButton
-              component={Link}
-              to="/services"
-              onClick={() => props.setSelectedIndex(0)}
-            >
-              <img src={backArrow} alt="back to service arrow" />
-            </IconButton>
+            <img src={backArrow} alt="back to service arrow" />
+          </IconButton>
+        </Grid>
+        <Grid item container direction="column" className={classes.heading}>
+          <Grid item>
+            <Typography variant="h2">Custom Software Development</Typography>
           </Grid>
-          <Grid item container direction="column" className={classes.heading}>
-            <Grid item>
-              <Typography variant="h2">Custom Software Development</Typography>
-            </Grid>
-            <Grid item>
-              <Typography variant="body1" paragraph>
-                Whether we’re replacing old software or inventing new solutions,
-                Arc Development is here to help your business tackle technology.
-              </Typography>
-              <Typography variant="body1" paragraph>
-                Using regular commercial software leaves you with a lot of stuff
-                you don’t need, without some of the stuff you do need, and
-                ultimately controls the way you work. Without using any software
-                at all you risk falling behind competitors and missing out on
-                huge savings from increased efficiency.
-              </Typography>
-              <Typography variant="body1" paragraph>
-                Our custom solutions are designed from the ground up with your
-                needs, wants, and goals at the core. This collaborative process
-                produces finely tuned software that is much more effective at
-                improving your workflow and reducing costs than generalized
-                options.
-              </Typography>
-              <Typography variant="body1" paragraph>
-                We create exactly what you what, exactly how you want it.
-              </Typography>
-            </Grid>
-          </Grid>
-          <Grid item className={classes.arrowContainer}>
-            <IconButton
-              component={Link}
-              to="/appdev"
-              onClick={() => props.setSelectedIndex(2)}
-            >
-              <img src={forwardArrow} alt="go to website development " />
-            </IconButton>
+          <Grid item>
+            <Typography variant="body1" paragraph>
+              Whether we’re replacing old software or inventing new solutions,
+              Arc Development is here to help your business tackle technology.
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Using regular commercial software leaves you with a lot of stuff
+              you don’t need, without some of the stuff you do need, and
+              ultimately controls the way you work. Without using any software
+              at all you risk falling behind competitors and missing out on huge
+              savings from increased efficiency.
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Our custom solutions are designed from the ground up with your
+              needs, wants, and goals at the core. This collaborative process
+              produces finely tuned software that is much more effective at
+              improving your workflow and reducing costs than generalized
+              options.
+            </Typography>
+            <Typography variant="body1" paragraph>
+              We create exactly what you what, exactly how you want it.
+            </Typography>
           </Grid>
         </Grid>
-        <Grid item container direction="row" justify="center" style={{marginTop:"10em", marginBottom:"10em"}}>
-          <Grid item container direction="column" md alignItems="center">
+        <Grid item className={classes.arrowContainer}>
+          <IconButton
+            component={Link}
+            to="/appdev"
+            onClick={() => props.setSelectedIndex(2)}
+          >
+            <img src={forwardArrow} alt="go to website development " />
+          </IconButton>
+        </Grid>
+      </Grid>
+      <Grid
+        item
+        container
+        direction="row"
+        justify="center"
+        style={{ marginTop: "10em", marginBottom: "10em" }}
+      >
+        <Grid item container direction="column" md alignItems="center">
+          <Grid item>
+            <Typography variant="h4">Save Energy</Typography>
+          </Grid>
+          <Grid item>
+            <img src={bulb} alt="Ligh Bulb" />
+          </Grid>
+        </Grid>
+        <Grid item container direction="column" md alignItems="center">
+          <Grid item>
+            <Typography variant="h4">Save Money</Typography>
+          </Grid>
+          <Grid item>
+            <img src={cash} alt="Ligh Bulb" />
+          </Grid>
+        </Grid>
+        <Grid item container direction="column" md alignItems="center">
+          <Grid item>
+            <Typography variant="h4">Save Time</Typography>
+          </Grid>
+          <Grid item>
+            <img src={stopWatch} alt="Ligh Bulb" />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item container direction={"row"}>
+        <Grid item container className={"itemContainer"}>
+          <Grid item container direction={"column"} md>
             <Grid item>
-              <Typography variant="h4">Save Energy</Typography>
+              <Typography variant="h4">Digital Documents & Data</Typography>
             </Grid>
             <Grid item>
-              <img src={bulb} alt="Ligh Bulb" />
+              <Typography variant="body1" paragraph>
+                Reduce Errors. Reduce Waste. Reduce Costs.
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1" paragraph>
+                Billions are spent annually on the purchasing, printing, and
+                distribution of paper. On top of the massive environmental
+                impact this has, it causes harm to your bottom line as well.
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1" paragraph>
+                By utilizing digital forms and documents you can remove these
+                obsolete expenses, accelerate your communication, and help the
+                Earth.
+              </Typography>
             </Grid>
           </Grid>
-          <Grid item container direction="column" md alignItems="center">
-            <Grid item>
-              <Typography variant="h4">Save Money</Typography>
-            </Grid>
-            <Grid item>
-              <img src={cash} alt="Ligh Bulb" />
-            </Grid>
+          <Grid item md>
+            <Lottie
+              options={documentOptions}
+              isStopped={true}
+              style={{ maxWidth: 275, maxHeight: 325, minHeight: 275 }}
+            />
           </Grid>
-          <Grid item container direction="column" md alignItems="center">
+        </Grid>
+        <Grid item container className={"itemContainer"}>
+          <Grid item md>
+            <Lottie
+              options={scaleOptions}
+              style={{ maxWidth: 260, maxHeight: 280 }}
+            />
+          </Grid>
+          <Grid item container direction={"column"} md>
             <Grid item>
-              <Typography variant="h4">Save Time</Typography>
+              <Typography variant="h4" align={"right"}>
+                Scale
+              </Typography>
             </Grid>
             <Grid item>
-              <img src={stopWatch} alt="Ligh Bulb" />
+              <Typography variant="body1" paragraph align={"right"}>
+                Whether you’re a large brand, just getting started, or taking
+                off right now, our application architecture ensures pain-free
+                growth and reliability.
+              </Typography>
             </Grid>
           </Grid>
         </Grid>
       </Grid>
+    </Grid>
   );
 };
 
